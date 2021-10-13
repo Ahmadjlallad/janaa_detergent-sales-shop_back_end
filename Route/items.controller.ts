aabb7584058c50266f75a504f9036436bd2e24d9
ahmadjlallad <ahmadjlad@hotmail.com>
2021-10-14 00:48:55 +0300

    -> progress 1 -create end points for items and receipt 2 - setup mongodb and the colections using mongoose

---
+++ Route/items.controller.ts
@@ -0,0 +1,63 @@
+import express, { Request, Response } from "express";
+import ItemsModel from "../mongo/itemsModel";
+const itemsRouter = express.Router();
+interface Query {
+  value?: string;
+  type?: string;
+}
+interface Items {
+  name?: string;
+  barcode?: string;
+  price?: number;
+  wholesalePrice?: number;
+}
+itemsRouter.get("/", async (req: Request, res: Response): Promise<void> => {
+  const { value, type }: Query = req.query;
+  try {
+    const allItems = await ItemsModel.find({});
+    if (type === "all") {
+      res.send(allItems);
+    } else {
+      const filteredItems = allItems.filter((item: Items) =>
+        item[type].includes(value)
+      );
+      res.send(filteredItems);
+    }
+  } catch (err) {
+    res.send(err);
+  }
+});
+itemsRouter.post("/", async (req: Request, res: Response) => {
+  try {
+    const newItem = await ItemsModel.create(req.body as Items);
+    res.send(newItem);
+  } catch (err) {
+    res.send(err);
+  }
+});
+itemsRouter
+  .route("/:id")
+  .put(async (req: Request, res: Response) => {
+    try {
+      const updatedItem = await ItemsModel.findByIdAndUpdate(
+        req.params.id,
+        req.body as Items
+      );
+      res.send(updatedItem);
+    } catch (err) {
+      res.send(err);
+    }
+  })
+  .delete(async (req: Request, res: Response) => {
+    try {
+      const deletedItem = await ItemsModel.findByIdAndDelete(req.params.id);
+      res.send(deletedItem);
+    } catch (err) {
+      res.send(err);
+    }
+  });
+
+itemsRouter.get("*", (_: Request, res: Response) => {
+  res.send("welcome to items try deferent /items/ point");
+});
+export default itemsRouter;
--- app.ts
+++ app.ts
@@ -1,13 +1,35 @@
 import express from "express";
 import { load } from "ts-dotenv";
 import cors from "cors";
+import mongoose, { ConnectOptions } from "mongoose";
+import itemRouter from "./Route/items.controller";
+import receiptRouter from "./Route/receipt.controller";
+interface mongooseOptions extends ConnectOptions {
+  useNewUrlParser: boolean;
+  useUnifiedTopology: boolean;
+}
 const app = express();
 app.use(cors());
 app.use(express.json());
 const env = load({
   PORT: Number,
+  DB: String,
+});
+mongoose.connect(env.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+} as mongooseOptions);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Connected to MongoDB");
 });
 
+app.use("/items", itemRouter);
+app.use("/receipt", receiptRouter);
+app.get("*", (_, res) => {
+  res.send("nothing here try /items | /recipe");
+});
 app.listen(env.PORT, () => {
   console.log("server is running " + env.PORT);
 });
